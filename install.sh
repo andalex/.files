f474fadbc37257e929fab720dee27966b6f7a8b9
and4alex <and4alex@gmail.com>
2019-07-11 15:49:37 +0000

    Make shell script easier to use via curl.

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+echo -e "\nDownloading from github...\n"
+
+git clone https://github.com/andalex/dotfiles.git
+
+cd dotfiles
 
 echo -e "\nCreating symlinks...\n"
 
 dotfiles=(
     "$PWD/.tmux.conf"
     "$PWD/.vimrc"
-    "$PWD/git/.bashrc"
     "$PWD/git/.gitconfig"
     "$PWD/git/.gitignore_global"
-    "$PWD/git/.zshrc"
     "$PWD/shell/.alias"
+    "$PWD/shell/.bashrc"
+    "$PWD/shell/.zshrc"
 )
 
 for file in "${dotfiles[@]}"; do
